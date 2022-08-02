cb5d14f275564a2caf0b3dcf714b568ee8214eeb
rodpedrassi <rodpedrassi@gmail.com>
2022-08-02 00:26:00 -0300

    requisito 6 quase feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
+import '../css/search.css';
 
 export default class Search extends Component {
   state = {
     search: '',
     isButtonDisabled: true,
-  }
+    loading: false,
+    listaArtistas: [],
+  };
 
   handleChange = ({ name, value }) => {
     this.setState(
       {
         [name]: value,
-      }, this.handleButtonDisable,
+      },
+      this.handleButtonDisable,
     );
   };
 
   handleButtonDisable = () => {
     const { search } = this.state;
-    console.log(search);
     const minLengthSearch = 2;
     const checkSearchInput = search.length < minLengthSearch;
     this.setState({
@@ -24,37 +30,66 @@ export default class Search extends Component {
     });
   };
 
-  onSearchClick = () => {
-    console.log('alooooooooooo');
-  }
+  onSearchClick = async (e) => {
+    e.preventDefault();
+    this.setState({ search: '', loading: true });
+    const { search } = this.state;
+    const newListaArtistas = await searchAlbumsAPIs(search);
+    this.setState({ loading: false });
+    this.setState({ listaArtistas: newListaArtistas });
+  };
 
   render() {
-    const { search, isButtonDisabled } = this.state;
+    const { search, isButtonDisabled, loading, listaArtistas } = this.state;
     return (
       <div data-testid="page-search">
-        <form action="" method="get">
-          <label htmlFor="search">
-            Pesquisar:
-            <input
-              type="text"
-              name="search"
-              id="search"
-              data-testid="search-artist-input"
-              placeholder="Digite o nome do artista"
-              value={ search }
-              onChange={ ({ target }) => this.handleChange(target) }
-            />
-          </label>
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ isButtonDisabled }
-            onClick={ this.onSearchClick }
-          >
-            Pesquisar
-
-          </button>
-        </form>
+        {loading ? (
+          <Loading />
+        ) : (
+          <form action="" method="get">
+            <label htmlFor="search">
+              Pesquisar:
+              <input
+                type="text"
+                name="search"
+                id="search"
+                data-testid="search-artist-input"
+                placeholder="Digite o nome do artista"
+                value={ search }
+                onChange={ ({ target }) => this.handleChange(target) }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isButtonDisabled }
+              onClick={ (e) => this.onSearchClick(e) }
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
+        <div className="content">
+          {listaArtistas === [] ? (
+            <h4>Nenhum álbum foi encontrado</h4>
+          ) : (
+            <ul>
+              {listaArtistas.map((artista) => (
+                <Link
+                  to={ `/album/${artista.collectionId}` }
+                  key={ artista.collectionId }
+                  data-testid={ `link-to-album-${artista.collectionId}` }
+                >
+                  <li className="artist-box" key={ artista.artistId }>
+                    <span>{artista.artistName}</span>
+                    <img src={ artista.artworkUrl100 } alt={ artista.artistName } />
+                    <span>{artista.collectionName}</span>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     );
   }
