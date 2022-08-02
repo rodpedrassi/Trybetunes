f676baebe8b02d4d70918b809940027f03bf1695
rodpedrassi <rodpedrassi@gmail.com>
2022-08-01 22:41:32 -0300

    requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+  state = {
+    search: '',
+    isButtonDisabled: true,
+  }
+
+  handleChange = ({ name, value }) => {
+    this.setState(
+      {
+        [name]: value,
+      }, this.handleButtonDisable,
+    );
+  };
+
+  handleButtonDisable = () => {
+    const { search } = this.state;
+    console.log(search);
+    const minLengthSearch = 2;
+    const checkSearchInput = search.length < minLengthSearch;
+    this.setState({
+      isButtonDisabled: checkSearchInput,
+    });
+  };
+
+  onSearchClick = () => {
+    console.log('alooooooooooo');
+  }
+
   render() {
+    const { search, isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        search
+        <form action="" method="get">
+          <label htmlFor="search">
+            Pesquisar:
+            <input
+              type="text"
+              name="search"
+              id="search"
+              data-testid="search-artist-input"
+              placeholder="Digite o nome do artista"
+              value={ search }
+              onChange={ ({ target }) => this.handleChange(target) }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+            onClick={ this.onSearchClick }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
